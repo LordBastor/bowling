bff3cb39f200fafb811e6483b06da47b61f641f3
Bastor <bobit_rex@yahoo.com>
2018-01-21 17:39:26 +0200

    Serializer metho field usage for score

--- bowling_game/serializers.py
+++ bowling_game/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 
 from .models import BowlingGame
 
-
 class BowlingGameSerializer(serializers.ModelSerializer):
+	score = serializers.SerializerMethodField()
+	
 	class Meta:
 		model = BowlingGame
 		fields = (
 			'id',
 			'user',
 			'rolls',
+			'score',
 		)
-
+	
+	def get_score(self, obj):
+		# how to do score
+		return 0
