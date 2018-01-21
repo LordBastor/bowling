5beaf5ff7f142454e7f22eb143efa81b2256631d
Bastor <bobit_rex@yahoo.com>
2018-01-21 17:04:39 +0200

    Added BowlingGame model not sure how many rolls in round is max so decided to store in csv

--- bowling_game/models.py
+++ bowling_game/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.conf import settings
+
+from django.core.validators import validate_comma_separated_integer_list
+
 
 class BaseModel(models.Model):
 	created_at = models.DateTimeField(auto_now_add=True)
@@ -7,3 +11,13 @@ class BaseModel(models.Model):
 	class Meta:
 		abstract = True
 		ordering = ['-updated_at']
+
+
+class BowlingGame(BaseModel):
+	user	 = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True)
+	rolls	 = models.CharField(
+		max_length=511,
+		blank=True,
+		null=True,
+		validators=[validate_comma_separated_integer_list]
+	)
