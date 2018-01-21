6f246ee922e59e86f59707891c5b9ffe821c6028
Bastor <bobit_rex@yahoo.com>
2018-01-21 17:07:42 +0200

    Added admin section for the game so we can track db contents

--- bowling_game/admin.py
+++ bowling_game/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BowlingGame
+
+
+class BowlingGameAdmin(admin.ModelAdmin):
+	list_display = (
+		'user',
+		'rolls',
+		'created_at',
+		'updated_at',
+	)
+admin.site.register(BowlingGame, BowlingGameAdmin)
